ff572396aa2bf0b66cac8c32cd1201c56183365f
Kumanan Murugesan <kumanan12@gmail.com>
2023-02-24 13:49:49 -0500

    async demo.

--- scripts/challenge-1.js
+++ scripts/challenge-1.js
@@ -1,28 +1,43 @@
 //[1,2,3,4,5,6,7,8,9,0]  => returns "(123) 456-7890"
-function createPhoneNumber(numbers){
-    //check for length
+// function createPhoneNumber(numbers){
+//     //check for length
   
-   if (!Array.isArray(numbers)) {
-        throw new Error("Only arrays of length 10 allowed")
-   }
-    if (numbers.length!=10) {
-        throw new Error("Only ten numbers are allowed");
-    }
-    console.log("string length ");
-    let first3Chars = numbers.slice(0,3).join("");
-    let second3Chars = numbers.slice(3,6).join("");
-    let third3Chars = numbers.slice(6).join("");
-    //output+=first3Chars+") "+second3Chars+"-"+third3Chars
-    let output= `(${first3Chars}) ${second3Chars}-${third3Chars}`;
-    console.log(output);
-}
+//    if (!Array.isArray(numbers)) {
+//         throw new Error("Only arrays of length 10 allowed")
+//    }
+//     if (numbers.length!=10) {
+//         throw new Error("Only ten numbers are allowed");
+//     }
+//     console.log("string length ");
+//     let first3Chars = numbers.slice(0,3).join("");
+//     let second3Chars = numbers.slice(3,6).join("");
+//     let third3Chars = numbers.slice(6).join("");
+//     //output+=first3Chars+") "+second3Chars+"-"+third3Chars
+//     let output= `(${first3Chars}) ${second3Chars}-${third3Chars}`;
+//     console.log(output);
+// }
 
-// createPhoneNumber([1,2,3,4,5,6,7,8,9,0]);
-try {
-    // createPhoneNumber("kkk");
-    createPhoneNumber([1,2,3,4,5,6,7,8,9,0]);
-    console.log("next line");
+// // createPhoneNumber([1,2,3,4,5,6,7,8,9,0]);
+// try {
+//     // createPhoneNumber("kkk");
+//     createPhoneNumber([1,2,3,4,5,6,7,8,9,0]);
+//     console.log("next line");
     
-} catch (error) {
-    console.error(error);
-}
+// } catch (error) {
+//     console.error(error);
+// }
+
+function isIsogram(str) {
+    str = str.toLowerCase();
+    let type = new Set();
+    for (let i = 0; i < str.length; i++) {
+      let letter = str[i];
+  if (type.has(letter)) {
+        return false;
+      }
+      type.add(letter);
+    }
+    return true;
+  }
+
+  console.log(isIsogram("kumu"));
--- scripts/fetch-async.js
+++ scripts/fetch-async.js
@@ -1,2 +1,23 @@
+const url = 'https://pets-v2.dev-apis.com/breeds?animal=dog';
 
+function add(n1,n2){
+    return new Promise((resolve) =>{resolve(n1+n2)}, (reject)=>{})
+    // return new Promise(resolve, reject) => function(){
+    //     resolve(n1+n2)
+    // };
+}
 
+ function getData() {
+    let result = await add(2,3);
+    console.log(result);
+}
+
+// async function getData() {
+//     console.log('step1');
+//     let response =  await fetch(url);
+//     let data =  await response.json();
+//     console.log(data);
+//     console.log('step2');
+// }
+
+getData();
