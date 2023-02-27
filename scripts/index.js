a8f6eeff40e285ac72022b1940ecb09a31c42c9e
Kumanan Murugesan <kumanan12@gmail.com>
2023-02-27 10:40:30 -0500

    responsible navbar draft1.

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+import { add,sub,divide } from "./my-math.js";
+
+console.log(add(2,3));
+console.log(sub(3,2));
+console.log(divide(12/3));
---
+++ scripts/my-math.js
@@ -0,0 +1,12 @@
+export function add(n1,n2) {
+    return n1 + n2;
+}
+
+export function sub(n1,n2){
+    return n1 -n2;
+
+}
+
+export function divide(n1,n2) {
+    return n1/n2;
+}
